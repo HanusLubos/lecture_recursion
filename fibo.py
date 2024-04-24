6ad35458156b98a07ded7e93e5da2c8ea0a47659
HanusLubos <lubos.hanus6@gmail.com>
2024-04-24 10:22:43 +0200

    fibo

--- fibo.py
+++ fibo.py
@@ -1,6 +1,20 @@
 
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
+
 def main():
-    pass
+    n = int(input("Zadej počet členů Fibonacciho posloupnosti: " ))
+    # seq = []
+    # for num in range(n):
+    #     seq.append(recursive_nth_fibo(num))
+    seq = [recursive_nth_fibo(num) for num in range(n)]
+    print(seq)
 
 
 if __name__ == "__main__":
